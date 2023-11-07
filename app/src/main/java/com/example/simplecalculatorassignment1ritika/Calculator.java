bff2026e88e6028b8d90a4364b53f49fcb53c71e
Ritika Bhargava <ritika.bhargava06@gmail.com>
2023-11-06 23:40:55 -0500

    standard calculator commit

---
+++ app/src/main/java/com/example/simplecalculatorassignment1ritika/Calculator.java
@@ -0,0 +1,112 @@
+package com.example.simplecalculatorassignment1ritika;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    ArrayList<String> inputArrayList;
+    boolean divisorIsZero=false;
+    public boolean push(String userInput){
+        boolean pushSuccessful = true;
+        inputArrayList = new ArrayList<>();
+        for (int i=0;i<userInput.length();i++) {
+            inputArrayList.add(String.valueOf(userInput.charAt(i)));
+        }
+        Log.d("inputArray", String.valueOf(inputArrayList));
+        if(validate()){
+            Log.d("validateInput", "Valid Input");
+        }else{
+            pushSuccessful=false;
+            Log.d("validateInput", "Invalid Input");
+        }
+        return pushSuccessful;
+    }
+
+    public int calculate(){
+        int result = 0;
+        int num1;
+        String operator;
+        int num2;
+
+        while (inputArrayList.size()>1){
+            num1 = Integer.parseInt(inputArrayList.get(0));
+            inputArrayList.remove(0);
+            operator = inputArrayList.get(0);
+            inputArrayList.remove(0);
+            num2 = Integer.parseInt(inputArrayList.get(0));
+            inputArrayList.remove(0);
+
+            switch (operator){
+                case "+":
+                    result = num1+num2;
+                    break;
+                case "-":
+                    result = num1-num2;
+                    break;
+                case "*":
+                    result = num1*num2;
+                    break;
+                case "/":
+                    if(num2==0){
+                        divisorIsZero = true;
+                    }else{
+                        result = num1/num2;
+                    }
+                    break;
+            }
+            inputArrayList.add(0, String.valueOf(result));
+        }
+        return  result;
+    }
+
+
+    boolean validate(){
+        boolean validInput = true;
+        boolean lastElement = false;
+
+        if(inputArrayList.size()>2&&inputArrayLengthIsOdd(inputArrayList.size())){
+            for(int index=0;index<inputArrayList.size();){
+
+                if(checkForOperator(inputArrayList.get(index))){
+                    validInput=false;
+                    break;
+                }else{
+                    if(lastElement){
+                        break;
+                    }
+                    index++;
+                    if(checkForOperator(inputArrayList.get(index))){
+                        index++;
+                        if (index==inputArrayList.size()-1){
+                            lastElement=true;
+                        }
+                    }else{
+                        validInput=false;
+                        break;
+                    }
+                }
+            }
+        }else{
+            validInput=false;
+        }
+        return validInput;
+    }
+
+    private boolean inputArrayLengthIsOdd(int length) {
+        boolean isodd=true;
+        if (length%2==0){
+            isodd=false;
+        }
+        return isodd;
+    }
+
+    private boolean checkForOperator(String e){
+        boolean isOperator=false;
+        if(e.equals("+")||e.equals("-")||e.equals("*")||e.equals("/")){
+            isOperator=true;
+        }
+        return isOperator;
+    }
+}
--- app/src/main/java/com/example/simplecalculatorassignment1ritika/MainActivity.java
+++ app/src/main/java/com/example/simplecalculatorassignment1ritika/MainActivity.java
@@ -3,12 +3,139 @@ package com.example.simplecalculatorassignment1ritika;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+    Button b9;
+    Button b0;
+    Button bplus;
+    Button bminus;
+    Button bmultiply;
+    Button bdivide;
+    Button bequals;
+    Button bclear;
+    TextView resultView;
+    Calculator cal = new Calculator();
+    boolean firstNum = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int bid=R.id.button1;
+    b1 = findViewById(R.id.button1);
+    b2 = findViewById(R.id.button2);
+    b3 = findViewById(R.id.button3);
+    b4 = findViewById(R.id.button4);
+    b5 = findViewById(R.id.button5);
+    b6 = findViewById(R.id.button6);
+    b7 = findViewById(R.id.button7);
+    b8 = findViewById(R.id.button8);
+    b9 = findViewById(R.id.button9);
+    b0 = findViewById(R.id.button0);
+    bplus = findViewById(R.id.buttonplus);
+    bminus = findViewById(R.id.buttonminus);
+    bmultiply = findViewById(R.id.buttonmultiply);
+    bdivide = findViewById(R.id.buttondivide);
+    bequals = findViewById(R.id.buttonequals);
+    bclear = findViewById(R.id.buttonclear);
+
+    resultView = findViewById(R.id.resultTextView);
+    b1.setOnClickListener(this);
+    b2.setOnClickListener(this);
+    b3.setOnClickListener(this);
+    b4.setOnClickListener(this);
+    b5.setOnClickListener(this);
+    b6.setOnClickListener(this);
+    b7.setOnClickListener(this);
+    b8.setOnClickListener(this);
+    b9.setOnClickListener(this);
+    b0.setOnClickListener(this);
+    bplus.setOnClickListener(this);
+    bminus.setOnClickListener(this);
+    bmultiply.setOnClickListener(this);
+    bdivide.setOnClickListener(this);
+    bequals.setOnClickListener(this);
+    bclear.setOnClickListener(this);
+
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.button1){
+            setResultView(b1);
+        }else if (v.getId()==R.id.button2){
+            setResultView(b2);
+        }else if (v.getId()==R.id.button3){
+            setResultView(b3);
+        }else if (v.getId()==R.id.button4){
+            setResultView(b4);
+        }else if (v.getId()==R.id.button5){
+            setResultView(b5);
+        }else if (v.getId()==R.id.button6){
+            setResultView(b6);
+        }else if (v.getId()==R.id.button7){
+            setResultView(b7);
+        }else if (v.getId()==R.id.button8){
+            setResultView(b8);
+        }else if (v.getId()==R.id.button9){
+            setResultView(b9);
+        }else if (v.getId()==R.id.button0){
+            setResultView(b0);
+        }else if (v.getId()==R.id.buttonplus){
+            setResultView(bplus);
+        }else if (v.getId()==R.id.buttonminus){
+            setResultView(bminus);
+        }else if (v.getId()==R.id.buttonmultiply){
+            setResultView(bmultiply);
+        }else if (v.getId()==R.id.buttondivide){
+            setResultView(bdivide);
+        }else if (v.getId()==R.id.buttonclear){
+            resultView.setText("");
+        }else if (v.getId()==R.id.buttonequals){
+            cal.divisorIsZero=false;
+            if(resultView.getText()!=""){
+                if (cal.push((String) resultView.getText())){
+                    int finalRes = cal.calculate();
+                    if (cal.divisorIsZero){
+                        //alert user that cant divide by 0
+                        Toast.makeText(this, "Cannot divide by 0", Toast.LENGTH_LONG).show();
+                        Log.d("Error", "Cant divide by 0");
+                    }else{
+                        resultView.setText(String.valueOf(finalRes));
+                        Log.d("final result", String.valueOf(finalRes));
+                    }
+                }else{
+                    //alert user to enter valid input
+                    Toast.makeText(this, "Please enter a valid input", Toast.LENGTH_LONG).show();
+                }
+            }else{
+                //alert the user to enter input
+                Toast.makeText(this,"Please provide an input", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    public void setResultView(Button pressedButton){
+        String resultText = (String) resultView.getText();
+        String newResult = resultText+pressedButton.getText();
+        resultView.setText(newResult);
+    }
+
 }
